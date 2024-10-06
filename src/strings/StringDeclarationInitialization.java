cf4b0c6d49e6092558c63a78ff14feeb784e0900
Abhinay Singh Negi <negia394@gmail.com>
2024-10-06 16:27:20 +0530

    Created string package and string declaration and initialization types code

---
+++ src/strings/StringDeclarationInitialization.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class StringDeclarationInitialization {
+
+    public static void main(String[] args){
+
+        // String literal
+        String literal = "Hello World!";
+
+        // Using new keyword to create String class object and passing string to the string constructor
+        String str = new String("Hello world");
+
+        System.out.println(str);
+    }
+}
